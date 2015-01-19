1fcb6b8bd50f8c09ba635042251dbf9b4048bbd0
Steve Eynon <3326741+SlimerDude@users.noreply.github.com>
2015-01-19 18:14:40 +0000

    Browser work

--- fan/public/fwt/Browser.fan
+++ fan/public/fwt/Browser.fan
@@ -2,83 +2,107 @@ using fwt
 using gfx
 
 ** (Widget) - 
+** A HTML and web browser widget. 
+** 'Browser' is a drop in replacement for the standard FWT [WebBrowser]`fwt::WebBrowser` with the following enhancements:
+** 
+**  - Javascript support
+**  - Title and status text support
+**  - 'WebBrowser' bug fixes - see [Topic #2069]`http://fantom.org/forum/topic/2069`
+** 
+** Use the 'html' and 'url' fields to load content into the browser:
+** 
+**   browser.html = "<html><body>Fantom-Factory<body></html>"
+** 
+**   browser.url = `http://www.fantomfactory.org/`
 ** 
 ** See [SWT Browser]`http://help.eclipse.org/indigo/topic/org.eclipse.platform.doc.isv/reference/api/org/eclipse/swt/browser/package-summary.html`
 class Browser : Pane {
 
-	private native	Widget?	browser
-	private 		Label?	statusBar
+	** Gets / sets the HTML in the current page.
+	**  
+	**   browser.html = "<html><body>Fantom-Factory<body></html>"
+	native	Str?	html
+
+	** Gets / sets the URL loaded in the browser.
+	**  
+	**   browser.url = `http://www.fantomfactory.org/`
+	native	Uri?	url
+	
+	** Whether javascript is enabled in the browser. 
+	** Note this only affects pages loaded *after* the value is set. 
+	native	Bool	javascriptEnabled
 	
 	@NoDoc
-	new make() : super() {
-		statusBar = Label() { it.text="status"}
-//		add(statusBar)
-	}
+	new make() : super() { }
 	
-	override Size prefSize(Hints hints := Hints.defVal) { Size(100, 100) }
-//	override Void onLayout() { }
-
-//	@NoDoc	// required by Pane
-//	override Size prefSize(Hints hints := Hints.defVal) {
-//		bottom := pref(this.statusBar)
-//		center := pref(this.browser)
-//
-//		w := (center.w).max(bottom.w)
-//		h := bottom.h + center.h
-//		return Size(w, h)
-//	}
-//
-//	private Size pref(Widget? w) {
-//		return w == null || !w.visible ? Size.defVal : w.prefSize(Hints.defVal)
-//	}
-//
 	@NoDoc	// required by Pane
-	override Void onLayout() {
-		x := 0; y := 0; w := size.w; h := size.h
+	override Size prefSize(Hints hints := Hints.defVal) { Size(100, 100) }
 
-		bottom := this.statusBar
-		if (bottom != null) {
-			prefh := bottom.prefSize(Hints(w, null)).h
-			bottom.bounds = Rect(x, y+h-prefh, w, prefh)
-			h -= prefh
-		}
+	@NoDoc	// required by Pane
+	override Void onLayout() { }
 
-//		center := this.browser
-//		if (center != null)
-//			center.bounds = Rect(x, y, w, h)
-		
-		echo("onLayout")
-	}
-	
-	
-//	** Callback when the user clicks a hyperlink.
-//	** The callback is invoked before the actual hyperlink.	
-//	** The event handler can modify the 'data' field with a new Uri or set to null to cancel the hyperlink.
-//	** This callback is *not* called if explicitly loaded via the `load` method.
-//	**
-//	** Event id fired:
-//	**	 - `EventId.hyperlink`
-//	**
-//	** Event fields:
-//	**	 - `Event.data`: the `sys::Uri` of the new page.
-//	once EventListeners onHyperlink() { EventListeners() }
-//
-//	** Navigate to the specified URI.
-//	native This load(Uri uri)
-//
-//	** Load the given HTML into the browser.
-//	native This loadStr(Str html)
-//
-//	** Refresh the current page.
-//	native This refresh()
-//
-//	** Stop any load activity.
-//	native This stop()
-//
-//	** Navigate to the previous session history.
-//	native This back()
-//
-//	** Navigate to the next session history.
-//	native This forward()
+	** Callback when the user clicks a hyperlink.
+	** The callback is invoked before the actual hyperlink.	
+	** The event handler can modify the 'data' field with a new URI or set to 'null' to cancel the hyperlink.
+	**
+	** Event id fired:
+	**	 - `EventId.hyperlink`
+	**
+	** Event fields:
+	**	 - `Event.data`: the `sys::Uri` of the new page.
+	once EventListeners onHyperlink() { EventListeners() }
+
+	** Callback when the page loading is complete. 
+	** DOM elements should be available for Javascript interaction at this time.
+	**
+	** Event id fired:
+	**	 - `EventId.unknown`
+	once EventListeners onLoad() { EventListeners() }
+
+	** Callback when the title text is available or is modified.
+	**
+	** Event id fired:
+	**	 - `EventId.unknown`
+	**
+	** Event fields:
+	**	 - `Event.data`: the title text.
+	once EventListeners onTitleText() { EventListeners() }
+
+	** Callback when the status bar text changes.
+	**
+	** Event id fired:
+	**	 - `EventId.unknown`
+	**
+	** Event fields:
+	**	 - `Event.data`: the status bar text.
+	once EventListeners onStatusText() { EventListeners() }
+
+	** Refresh the current page.
+	native This refresh()
+
+	** Stop any load activity.
+	native This stop()
+
+	** Navigate to the previous session history.
+	native This back()
+
+	** Navigate to the next session history.
+	native This forward()
+
+	** Returns the result, if any, of executing the specified script.
+	** 
+	** The the last Javascript statement should be a 'return' statement:
+	** 
+	**   browser.evaluate("alert('Hello Mum!'); return document.title;")
+	native Obj? evaluate(Str script)
+
+	** Executes the specified script. 
+	** If document-defined functions or properties are accessed by the script then this method 
+	** should not be invoked until the document has finished loading.
+	** 
+	**   browser.execute("alert('Hello Mum!');")
+	** 
+	** Throws 'Err' if the script did not complete successfully.
+	native Void execute(Str script)
 
 }
--- java/fan/afReflux/BrowserPeer.java
+++ java/fan/afReflux/BrowserPeer.java
@@ -3,130 +3,153 @@ package fan.afReflux;
 import fan.sys.*;
 import fan.fwt.Label;
 import fan.fwt.PanePeer;
+import fan.fwt.Prop;
+
 import org.eclipse.swt.*;
 import org.eclipse.swt.browser.*;
-//import org.eclipse.swt.layout.*;
+import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.widgets.*;
 
-public class BrowserPeer extends PanePeer implements LocationListener {
+public class BrowserPeer extends PanePeer implements LocationListener, TitleListener, StatusTextListener, ProgressListener, OpenWindowListener {
 
-	Uri loadUri;
-	String loadStr;
-	boolean explicitLoad;
+	String	statusText;
+	Uri		nextUrl;
 	
-	fan.fwt.Widget	fanBrowser;
-
 	public static BrowserPeer make(fan.afReflux.Browser self) throws Exception {
 		BrowserPeer peer = new BrowserPeer();
 		((fan.fwt.Widget)self).peer = peer;
 		peer.self = self;
 		return peer;
 	}
-
-	public fan.fwt.Widget browser(fan.afReflux.Browser self) {
-		return fanBrowser;
-	}
-	public void browser(fan.afReflux.Browser self, fan.fwt.Widget browser) {
-		this.fanBrowser = browser;
-	}
 	
 	public Widget create(Widget parent) {
 		org.eclipse.swt.browser.Browser browser = new org.eclipse.swt.browser.Browser((Composite) parent, 0);
 		
-		browser.setText("Steve");
-		
-		Browser self = (Browser) this.self;
+		browser.addLocationListener(this);
+		browser.addTitleListener(this);
+		browser.addStatusTextListener(this);
+		browser.addProgressListener(this);
+		browser.addOpenWindowListener(this);
 
-//		b.addLocationListener(this);
-//		if (loadUri != null) load((Browser)self, loadUri);
-//		else if (loadStr != null) loadStr((Browser)self, loadStr);
-		return b;
+		return browser;
 	}
 
-	// ---- Commands ------------------------------------------------------------------------------
 	
-//	public Browser load(Browser self, Uri uri) {
-//		Browser b = (Browser) this.control;
-//		if (b == null) { loadUri = uri; return self; }
-//		explicitLoad = true;
-//		
-//		try {
-//			b.setUrl(uri.toString());
-//			return self;
-//			
-//		} finally {
-//			explicitLoad = false;
-//		}
-//	}
-//
-//	public Browser loadStr(Browser self, String html) {
-//		Browser b = (Browser) this.control;
-//		if (b == null) { loadStr = html; return self; }
-//		explicitLoad = true;
-//		
-//		try {
-//			b.setText(html);
-//			return self;
-//			
-//		} finally {
-//			explicitLoad = false;
-//		}
-//	}
-//
-//	public Browser refresh(Browser self) {
-//		Browser b = (Browser) this.control;
-//		if (b != null) b.refresh();
-//		return self;
-//	}
-//
-//	public Browser stop(Browser self) {
-//		Browser b = (Browser) this.control;
-//		if (b != null) b.stop();
-//		return self;
-//	}
-//
-//	public Browser back(Browser self) {
-//		Browser b = (Browser) this.control;
-//		if (b != null) b.back();
-//		return self;
-//	}
-//
-//	public Browser forward(Browser self) {
-//		Browser b = (Browser) this.control;
-//		if (b != null) b.forward();
-//		return self;
-//	}
+	public Uri url(Browser self) { String u = url.get(); return "".equals(u) ? null : Uri.fromStr(u); }
+	public void url(Browser self, Uri v) { url.set(v == null ? "" : v.toStr()); }
+	public final Prop.StrProp url = new Prop.StrProp(this, "") {
+		public String get(Widget w) { return ((org.eclipse.swt.browser.Browser) w).getUrl(); }
+		public void set(Widget w, String v) { nextUrl = Uri.fromStr(v); ((org.eclipse.swt.browser.Browser) w).setUrl(v); }
+	};
+
+	public String html(Browser self) { String t = html.get(); return "".equals(t) ? null : t; }
+	public void html(Browser self, String v) { html.set(v == null ? "" : v); }
+	public final Prop.StrProp html = new Prop.StrProp(this, "") {
+		public String get(Widget w) { return ((org.eclipse.swt.browser.Browser) w).getText(); }
+		public void set(Widget w, String v) { nextUrl = Uri.fromStr("about:blank"); ((org.eclipse.swt.browser.Browser) w).setText(v, true); }	// trusted text
+	};
+	
+	public boolean javascriptEnabled(Browser self) { return javascriptEnabled.get(); }
+	public void javascriptEnabled(Browser self, boolean v) { javascriptEnabled.set(v); }
+	public final Prop.BoolProp javascriptEnabled = new Prop.BoolProp(this, true) {
+		public boolean get(Widget w) { return ((org.eclipse.swt.browser.Browser) w).getJavascriptEnabled(); }
+		public void set(Widget w, boolean v) { ((org.eclipse.swt.browser.Browser) w).setJavascriptEnabled(v); }
+	};
 
 	// ---- Events --------------------------------------------------------------------------------
 
+	public void changed (LocationEvent event) { }
 	public void changing(LocationEvent event) {
-//		// don't handle event if load() called
-//		if (explicitLoad) return;
-//
-//		// map to a Uri, this is a bit hacky, but it appears that links
-//		// on local file system give us back an OS path instead of a URI;
-//		// we need to handle the Windows case of "c:\..." since the drive
-//		// will be interpretted as a scheme
-//		String loc = event.location;
-//		if (loc.startsWith("file:///")) loc = "file:/" + loc.substring(8);
-//		if (loc.startsWith("file://"))	loc = "file:/" + loc.substring(7);
-//		Uri uri = Uri.fromStr(loc);
-//		if (uri.scheme() == null || uri.scheme().length() == 1)
-//			uri = File.os(loc).normalize().uri();
-//
-//		fan.fwt.Browser self = (fan.fwt.Browser)this.self;
-//		fan.fwt.Event fe = event(EventId.hyperlink, uri);
-//		self.onHyperlink().fire(fe);
-//		if (fe.data == null) {
-//		
-//			event.doit = false;
-//		}
-//		else {
-//		
-//			event.doit = true;
-//			event.location = fe.data.toString();
-//		}
+		// map to a Uri, this is a bit hacky, but it appears that links
+		// on local file system give us back an OS path instead of a URI;
+		// we need to handle the Windows case of "c:\..." since the drive
+		// will be interpretted as a scheme
+		String loc = event.location;
+		if (loc.startsWith("file:///")) loc = "file:/" + loc.substring(8);
+		if (loc.startsWith("file://"))	loc = "file:/" + loc.substring(7);
+		Uri uri = Uri.fromStr(loc);
+		if (uri.scheme() == null || uri.scheme().length() == 1)
+			uri = File.os(loc).normalize().uri();
+
+		// don't handle ourselves!
+		if (uri.equals(nextUrl)) {
+			return;
+		}		
+
+		Browser self = (Browser) this.self;
+		fan.fwt.Event fe = event(fan.fwt.EventId.hyperlink, uri);
+		self.onHyperlink().fire(fe);
+		
+		if (fe.data == null) {
+			event.doit = false;
+		} else {
+			event.doit = true;
+			// meh, event.location is input only, we can't change it
+			event.location = fe.data.toString();
+		}
+	}
+	
+	public void changed  (ProgressEvent event) { }
+	public void completed(ProgressEvent event) {
+		fan.fwt.Event fe = event(fan.fwt.EventId.unknown, null);
+		((Browser) this.self).onLoad().fire(fe);
 	}
 
-	public void changed(LocationEvent event) { }
+	public void changed(StatusTextEvent event) {
+		// we get a shed load of repeat events - so lets filter them out
+		if (!event.text.equals(statusText)) {
+			statusText = event.text; 
+			fan.fwt.Event fe = event(fan.fwt.EventId.unknown, event.text);
+			((Browser) this.self).onStatusText().fire(fe);
+		}
+	}
+	
+	public void changed(TitleEvent event) {
+		fan.fwt.Event fe = event(fan.fwt.EventId.unknown, event.title);
+		((Browser) this.self).onTitleText().fire(fe);
+	}
+
+	public void open(WindowEvent event) {
+		// called when user selects 'open in new window'
+//		event.required = false;	// true to launch in IE, false to ignore
+//		event.browser = null;
+	}	
+
+	// ---- Commands ------------------------------------------------------------------------------
+
+	public Browser refresh(Browser self) {
+		if (browser() != null) browser().refresh();
+		return self;
+	}
+
+	public Browser stop(Browser self) {
+		if (browser() != null) browser().stop();
+		return self;
+	}
+
+	public Browser back(Browser self) {
+		if (browser() != null) browser().back();
+		return self;
+	}
+
+	public Browser forward(Browser self) {
+		if (browser() != null) browser().forward();
+		return self;
+	}
+
+	public Object evaluate(Browser self, String script) {
+		return browser() != null ? browser().evaluate(script) : null;
+	}
+
+	public void execute(Browser self, String script) {
+		if (browser() != null) {
+			boolean res = browser().execute(script);
+			if (!res)
+				throw new RuntimeException("Script was not successful:\n\n" + script);
+		}
+	}
+	
+	private org.eclipse.swt.browser.Browser browser() {
+		return (org.eclipse.swt.browser.Browser) this.control();
+	}
 }
